10eaeacfbe4a6b2880aba33b535dada8652ea808
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2024-03-23 04:36:52 -0300

    feat: create component GridText

---
+++ src/components/GridText/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { Title } from '../Heading/styles';
+import { Container as TextComponent } from '../TextComponent/styles';
+
+export const Container = styled.div`
+  ${TextComponent} {
+    margin-bottom: ${({ theme }) => theme.spacings.xhuge};
+  }
+`;
+export const Grid = styled.div`
+  counter-reset: grid-counter;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: ${({ theme }) => theme.spacings.large};
+`;
+export const GridElement = styled.div`
+  ${Title} {
+    position: relative;
+    left: 5rem;
+  }
+
+  ${Title}::before {
+    counter-increment: grid-counter;
+    content: counter(grid-counter);
+    position: absolute;
+    font-size: 7rem;
+    top: -3rem;
+    left: -5rem;
+    transform: rotate(5deg);
+  }
+`;
