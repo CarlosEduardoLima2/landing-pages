d6afe3c6784d75125f1f9a6410d9b53c6a7bc4b1
Carlos Eduardo <carloseduardolimabastosm@gmail.com>
2024-03-26 07:14:46 -0300

    feat: create component Footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.footer`
+  text-align: center;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    font-size: ${({ theme }) => theme.fonts.sizes.small};
+  }
+`;
